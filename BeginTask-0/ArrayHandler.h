3910dc19224fbcaccb7317e4a1090d317b2f4373
YusupkinaRita <yusupkina.rita@gmail.com>
2024-10-02 13:46:17 +0300

    ddd

--- BeginTask-0/ArrayHandler.h
+++ BeginTask-0/ArrayHandler.h
@@ -1,31 +1,59 @@
-
+#include <cstring>
+#include <limits>
 template<typename T>
 class ArrayHandler {
 private:
     size_t _size;
+    size_t _count;
     T* _array;
+    T _max;
+    T _min;
 public:
     ArrayHandler(size_t size = 10) {
+    _size=size;
+    _array=new T[_size];
+    _max=std::numeric_limits<T>::min();
+    _min=std::numeric_limits<T>::max();
+    _count=0;
 
     }
 
     void AppendElem(T elem) {
+    if(_count==_size){
+    _size=_size*5;
+    T* temp_arr=new T[_size];
+    std::memcpy(temp_arr,_array,_size*sizeof(T));
+    delete[] _array;
+    _array=temp_arr;
+    }
+    _array[_count]=elem;
+
+    if(_array[_count]>_max)
+    _max=_array[_count];
+    if(_array[_count]<_min)
+    _min=_array[_count];
+    _count++;
 
     }
 
     bool IsContains(T elem) {
+        for(size_t i=0;i<_size;i++){
+            if(_array[i]==elem)
+            return true;
+        }
         return false;
     }
 
     T GetMax() {
-        return 0;
+        return _max;
     }
 
     T GetMin() {
-        return 0;
+        return _min;
     }
 
     ~ArrayHandler() {
+        delete[]_array;
 
     }
 
